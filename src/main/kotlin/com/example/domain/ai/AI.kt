cdeab2e95125964ef25e716015bd912eeabe9364
dohwi <dohwe0978@gmail.com>
2025-08-02 23:05:25 +0900

    feat :: ai

---
+++ src/main/kotlin/com/example/domain/ai/AI.kt
@@ -0,0 +1,16 @@
+package com.example.domain.ai
+
+import com.example.global.base.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "tbl_ai")
+class AI(
+    val name: String,
+    val description: String,
+    @Column(length = 1000)
+    val prompt: String
+):BaseEntity() {
+}
---
+++ src/main/kotlin/com/example/domain/ai/persistance/AiRepository.kt
@@ -0,0 +1,8 @@
+package com.example.domain.ai.persistance
+
+import com.example.domain.ai.AI
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface AiRepository: JpaRepository<AI, UUID> {
+}

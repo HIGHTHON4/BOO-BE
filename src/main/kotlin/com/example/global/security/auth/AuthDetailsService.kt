4085ee26756cda02804bfeb66034f0bdf69cc7f2
dohwi <dohwe0978@gmail.com>
2025-08-02 22:59:12 +0900

    feat :: auth

---
+++ src/main/kotlin/com/example/global/security/auth/AuthDetailsService.kt
@@ -0,0 +1,17 @@
+package com.example.global.security.auth
+
+import com.example.debatematch.domain.user.facade.UserFacade
+import com.example.global.security.auth.AuthDetails
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Component
+
+@Component
+class AuthDetailsService(
+    private val userFacade: UserFacade
+) : UserDetailsService {
+    override fun loadUserByUsername(accountId: String): UserDetails {
+        val user = userFacade.getUserByAccountIdOrThrow(accountId)
+        return AuthDetails(user.accountId)
+    }
+}

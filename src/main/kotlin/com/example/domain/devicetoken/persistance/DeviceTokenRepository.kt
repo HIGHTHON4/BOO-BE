cd12f831c956998a4fd1a8ad6b4ba86a6eade9bf
dohwi <dohwe0978@gmail.com>
2025-08-02 23:17:55 +0900

    feat :: device token repository

---
+++ src/main/kotlin/com/example/domain/devicetoken/persistance/DeviceTokenRepository.kt
@@ -0,0 +1,10 @@
+package com.example.domain.devicetoken.persistance
+
+import com.example.domain.devicetoken.DeviceToken
+import com.example.domain.user.enum.OS
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface DeviceTokenRepository: JpaRepository<DeviceToken, UUID> {
+    fun findByUserIdAndOs(userId: UUID, os: OS): DeviceToken?
+}

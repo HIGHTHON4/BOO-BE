69d2cac12cb235827ee506b330cb37ba2bb3b3ae
dohwi <dohwe0978@gmail.com>
2025-08-02 23:12:01 +0900

    feat :: ghost stroy

---
+++ src/main/kotlin/com/example/domain/ghoststory/GhostStory.kt
@@ -0,0 +1,20 @@
+package com.example.domain.ghoststory
+
+import com.example.domain.report.Report
+import com.example.global.base.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "tbl_ghoststory")
+class GhostStory(
+    val title: String,
+    @Column(length = 2000)
+    val content: String,
+
+    @ManyToOne
+    val report: Report,
+): BaseEntity() {
+}

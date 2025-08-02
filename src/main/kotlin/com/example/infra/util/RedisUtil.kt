6e2bd18a672413647f22be78232e57a8226480ba
dohwi <dohwe0978@gmail.com>
2025-08-03 07:04:12 +0900

    feat :: redis util

---
+++ src/main/kotlin/com/example/infra/util/RedisUtil.kt
@@ -0,0 +1,24 @@
+package com.example.infra.util
+
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+@Component
+class RedisUtil(
+    private val stringRedisTemplate: StringRedisTemplate
+)  {
+   fun getData(key: String): String? {
+        val valueOperations = stringRedisTemplate.opsForValue()
+        return valueOperations[key]
+    }
+
+     fun setDataExpire(key: String, value: String, duration: Long) {
+        val valueOperations = stringRedisTemplate.opsForValue()
+        valueOperations.set(key, value, duration, TimeUnit.SECONDS)
+    }
+
+         fun deleteData(key: String) {
+        stringRedisTemplate.delete(key)
+    }
+}

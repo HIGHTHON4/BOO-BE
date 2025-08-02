0d8c3da1d6efb0bee4cfd2bca2e01463fc470d8e
dohwi <dohwe0978@gmail.com>
2025-08-03 01:29:27 +0900

    feat :: level 메서드 생성

--- src/main/kotlin/com/example/domain/report/enum/FearLevel.kt
+++ src/main/kotlin/com/example/domain/report/enum/FearLevel.kt
@@ -1,5 +1,9 @@
 package com.example.domain.report.enum
 
 enum class FearLevel {
-    F, E, D, C, B, A, S
+    F, E, D, C, B, A, S;
+
+    fun getFearLevel(ordinal: Int): FearLevel {
+        return entries.getOrNull(ordinal)!!
+    }
 }

1177ed505016dfdf427c34137866148038e81d4b
dohwi <dohwe0978@gmail.com>
2025-08-02 23:07:27 +0900

    feat :: controller

---
+++ src/main/kotlin/com/example/domain/user/presentation/UserController.kt
@@ -0,0 +1,41 @@
+package com.example.debatematch.domain.user.presentation
+
+import com.example.debatematch.domain.user.presentation.dto.UserLoginRequest
+import com.example.domain.user.presentation.dto.UserSignUpRequest
+import com.example.debatematch.domain.user.service.UserLoginService
+import com.example.debatematch.domain.user.service.UserResignService
+import com.example.debatematch.domain.user.service.UserSignUpService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@RestController
+@RequestMapping("/user")
+class UserController(
+    private val userSignUpService: UserSignUpService,
+    private val userResignService: UserResignService,
+    private val userLoginService: UserLoginService,
+
+) {
+    @PostMapping("/signup")
+    fun signUp(
+        @RequestBody userSignUpRequest: UserSignUpRequest
+    ): UUID? {
+        return userSignUpService.execute(userSignUpRequest)
+    }
+
+    @DeleteMapping("/resign")
+    fun resign() {
+        userResignService.execute()
+    }
+
+    @PostMapping("/login")
+    fun login(
+        @RequestBody request: UserLoginRequest
+    ) = userLoginService.execute(request)
+
+
+}

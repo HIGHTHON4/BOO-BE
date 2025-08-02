29ca888b6d997175b7f92b58427a2fed40fe90b7
dohwi <dohwe0978@gmail.com>
2025-08-02 23:18:20 +0900

    feat :: os 추가

--- src/main/kotlin/com/example/domain/user/presentation/dto/UserSignUpRequest.kt
+++ src/main/kotlin/com/example/domain/user/presentation/dto/UserSignUpRequest.kt
@@ -1,6 +1,11 @@
 package com.example.domain.user.presentation.dto
 
+import com.example.domain.devicetoken.DeviceToken
+import com.example.domain.user.enum.OS
+
 data class UserSignUpRequest(
     val accountId: String,
     val password: String,
+    val deviceToken: String,
+    val os: OS
 )

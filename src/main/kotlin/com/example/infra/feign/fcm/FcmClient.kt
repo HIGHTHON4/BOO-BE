70b266a791efe4b781a24fd9bb5ac1cc4e3a5f04
변도휘 <dohwe0978@gmail.com>
2025-08-03 09:55:52 +0900

    Apply .gitignore rules and untrack ignored files

--- src/main/kotlin/com/example/infra/feign/fcm/FcmClient.kt
+++ src/main/kotlin/com/example/infra/feign/fcm/FcmClient.kt
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 
-@FeignClient(name = "fcm", url = "\${firebase.messaging.url.host}")
+@FeignClient(name = "fcm", url = "https://fcm.googleapis.com/v1/projects/boo-9b814/messages:send")
 interface FcmClient {
 
-    @PostMapping("\${firebase.messaging.url.path}")
+    @PostMapping(
+        headers = ["Content-Type=application/json"]
+    )
     fun sendMessage(
         @RequestHeader("Authorization") authorization: String,
         @RequestBody request: FcmMessage
